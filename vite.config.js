8b06adbb965858ebf404de5d43b4b8437e179cb1
llaseratheo <llasera.theo@gmail.com>
2025-10-22 21:57:19 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  root: '.',
-  publicDir: 'public',
+  root: '.', // le point de départ du projet
+  publicDir: 'public', // les fichiers statiques
   build: {
     outDir: 'dist',
   },
